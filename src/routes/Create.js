7c160fd76004cad6c70bacab3e0b12fa1a88b672
Zachary Habib <zach.habib16@gmail.com>
2021-11-11 11:56:12 -0700

    Fixed form

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -1,20 +1,59 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useFormik } from 'formik'
 
 const style = {
 	margin: "0 auto"
 }
 
+// const Create = () => {
+// 	return (
+// 		<div style={style}>
+// 			<h1>Create Recipe</h1>
+// 			<Formik
+// 				initalValues={{}}
+// 			>
+// 				<Form>
+// 					<input type="text" id="title" name="title"/>
+// 				</Form>
+// 			</Formik>
+// 		</div>
+// 	)
+// }
+
 const Create = () => {
+	const formik = useFormik({
+		initialValues: {
+			title: '',
+			desc: ''
+		},
+		onSubmit: values => {
+			alert(JSON.stringify(values, null, 2))
+		}
+	})
+
 	return (
-		<div style={style}>
+		<div>
 			<h1>Create Recipe</h1>
-			<Formik
-				initalValues={{ email: 'test'}}
-			>
-				<Form>
-					<Field type="email" name="email" />
-				</Form>
-			</Formik>
+			<form onSubmit={formik.handleSubmit}>
+				<label htmlFor="title">Title: </label>
+				<input
+					id="title"
+					name="title"
+					type="text"
+					onChange={formik.handleChange}
+					value={formik.values.title}
+				/>
+				<br/>
+				<label htmlFor="desc">Description: </label>
+				<input
+					id="desc"
+					name="desc"
+					type="text"
+					onChange={formik.handleChange}
+					value={formik.values.desc}
+				/>
+				<br/>
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	)
 }

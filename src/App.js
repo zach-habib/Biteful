561fa7570b95a7a1b68768118d1afe6223416492
Zachary Habib <zach.habib16@gmail.com>
2021-11-23 22:09:53 -0700

    firebase auth:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import RecipeView from "./components/RecipeView"
 import AddButton from './components/AddButton'
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDAQyMXDa-HVItPIQQjGT5qZu9GxBnhy8E",
@@ -14,18 +15,46 @@ const firebaseConfig = {
   measurementId: "G-8EX0CTJH0B"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+let user = auth.currentUser;
+
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    // TODO: Sync user with react state
+    user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+function SignIn() {
+  signInWithRedirect(auth, provider)
+}
 
 function App() {
+
   return (
     <div className="App">
       <h1>Recipe App</h1>
-
-      <RecipeView />
+      {/* <RecipeView /> */}
       <Link to="/create">
         <AddButton />
       </Link>
+      {user ? <RecipeView /> : <button onClick={SignIn}>Sign In</button>}
+
     </div>
   );
 }

b093c1f7c139d7d76cb9ad30d5eef79c549790f5
Zachary Habib <zach.habib16@gmail.com>
2021-11-23 21:28:09 -0700

    Updated firebase config

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import { Link } from "react-router-dom"
 import './App.css';
 import RecipeView from "./components/RecipeView"
 import AddButton from './components/AddButton'
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAU_7DKDVODfNF-ZuxO5LbGvdASQi03Cu4",
-  authDomain: "recipe-app-a0098.firebaseapp.com",
-  projectId: "recipe-app-a0098",
-  storageBucket: "recipe-app-a0098.appspot.com",
-  messagingSenderId: "524216708649",
-  appId: "1:524216708649:web:c1fcd8251a4ea8cc40f638",
-  measurementId: "G-KQ0KB8FZDR"
+  apiKey: "AIzaSyDAQyMXDa-HVItPIQQjGT5qZu9GxBnhy8E",
+  authDomain: "recipe-app-8f059.firebaseapp.com",
+  projectId: "recipe-app-8f059",
+  storageBucket: "recipe-app-8f059.appspot.com",
+  messagingSenderId: "204166007972",
+  appId: "1:204166007972:web:30c360b0934be2085daa31",
+  measurementId: "G-8EX0CTJH0B"
 };
 
 // Initialize Firebase

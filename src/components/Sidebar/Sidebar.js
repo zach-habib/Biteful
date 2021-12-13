5c88359031f2c0edbd698d194295c30863efd956
Zachary Habib <zach.habib16@gmail.com>
2021-12-13 16:03:59 -0700

    Add buttons on sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,19 @@
+import Button from '@mui/material/Button'
+import { Link } from 'react-router-dom'
 import SidebarItem from "./SidebarItem"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <SidebarItem />
-      <SidebarItem />
-      <SidebarItem />
+      <Link to="/">
+        <Button variant="contained">Home</Button>
+      </Link>
+      <Link to="login">
+        <Button variant="contained">Login</Button>
+      </Link>
+      <Link to="create">
+        <Button variant="contained">create</Button>
+      </Link>
     </div>
   )
 }

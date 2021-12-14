29232365f88a1d91c47cc2291220d9ee3d8fc5aa
Zachary Habib <zach.habib16@gmail.com>
2021-12-13 21:54:46 -0700

    Tab Panel for create page

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -1,18 +1,39 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useFirebaseAuth } from '../FirebaseAuthProvider'
-import Step from '../components/create/Step'
+import Box from '@mui/material/Box'
+import { TabPanel, TabContext, TabList } from '@mui/lab';
+import { Tab, Tabs } from '@mui/material'
 import Section from '../components/create/Section'
-import recipesData from './../components/recipesData'
 import Sidebar from "../components/sidebar/Sidebar"
 
 const Create = () => {
+	const [value, setValue] = useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+
+
 	return (
 		<div className="main">
 			<Sidebar />
 			<div className="content">
 				<h1>Create Recipe</h1>
-				<Section />
+
+				<TabContext value={value}>
+					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+						<TabList onChange={handleChange} aria-label="lab API tabs example">
+							<Tab label="Overview" value="1" />
+							<Tab label="Ingredients" value="2" />
+							<Tab label="Prep" value="3" />
+							<Tab label="Instructions" value="4" />
+						</TabList>
+					</Box>
+					<TabPanel value="1"><Section /></TabPanel>
+					<TabPanel value="2"></TabPanel>
+					<TabPanel value="3"></TabPanel>
+					<TabPanel value="4"><Section /></TabPanel>
+				</TabContext>
 			</div>
 		</div>
 	)

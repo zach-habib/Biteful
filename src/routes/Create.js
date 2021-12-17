1102e3ee6085f1ff6aacae9e6b7a87101a1da114
Zachary Habib <zach.habib16@gmail.com>
2021-12-17 11:51:02 -0700

    Change overview to only one step

--- src/routes/Create.js
+++ src/routes/Create.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box'
 import { TabPanel, TabContext, TabList } from '@mui/lab';
 import { Tab } from '@mui/material'
 import Section from './../components/create/Section'
+import Step from './../components/create/Step'
 import Sidebar from "./../components/sidebar/Sidebar"
 
 // const recipe = {
@@ -12,7 +13,7 @@ import Sidebar from "./../components/sidebar/Sidebar"
 // 	instructions: {}
 // }
 
-const stockRecipe = [{ items: [] }, { items: [] }, { items: [] }, { items: [] }];
+const stockRecipe = [{}, { items: [] }, { items: [] }];
 
 const Create = () => {
 	const [value, setValue] = useState("0");
@@ -41,14 +42,12 @@ const Create = () => {
 						<TabList onChange={handleTabChange} aria-label="lab API tabs example">
 							<Tab label="Overview" value="1" />
 							<Tab label="Ingredients" value="2" />
-							<Tab label="Prep" value="3" />
-							<Tab label="Instructions" value="4" />
+							<Tab label="Directions" value="3" />
 						</TabList>
 					</Box>
-					<TabPanel value="1"><Section id={0} value={recipe[0]} onChange={handleData} /></TabPanel>
+					<TabPanel value="1"><Step id={0} value={recipe[0]} onChange={handleData} /></TabPanel>
 					<TabPanel value="2"><Section id={1} value={recipe[1]} onChange={handleData} type="ingredients" /></TabPanel>
 					<TabPanel value="3"><Section id={2} value={recipe[2]} onChange={handleData} /></TabPanel>
-					<TabPanel value="4"><Section id={3} value={recipe[3]} onChange={handleData} /></TabPanel>
 				</TabContext>
 
 				<p>

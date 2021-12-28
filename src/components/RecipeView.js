b0a88828b322079ee0401fdc0f6983353d2c3493
Zachary Habib <zach.habib16@gmail.com>
2021-12-27 21:15:39 -0700

    convert recipeview to use material grid

--- src/components/RecipeView.js
+++ src/components/RecipeView.js
@@ -1,21 +1,31 @@
 import React from "react"
+import { Grid } from "@mui/material"
 import RecipeCard from "./RecipeCard"
 
 //props contains array of recipes
-
 const RecipeView = (props) => {
   const recipeItems = props.recipes.map(recipe => {
-    return <RecipeCard
-      key={recipe.id}
-      recipe={recipe}
-      onDelete={props.onDelete}
-    />
+    return (
+      <Grid item xs={2} md={2}>
+        <RecipeCard
+          key={recipe.id}
+          recipe={recipe}
+          onDelete={props.onDelete}
+        />
+      </Grid>
+    )
   })
 
   return (
-    <div className="recipe-view">
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+    >
       {recipeItems}
-    </div>
+    </Grid>
   )
 }
 

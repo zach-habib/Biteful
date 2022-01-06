9e945d68b57fc806fa5469699450ab246ad9a0ba
Zachary Habib <zach.habib16@gmail.com>
2022-01-05 21:46:16 -0700

    Sidebar styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,40 +1,46 @@
 import Button from '@mui/material/Button'
+import { Paper } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom'
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <Button className="sidebar-button"
-        variant="contained"
-        sx={{ m: 0.5 }}
-        component={Link}
-        to="/"
-        label="Home"
-        value="home"
-      >
-        Home
-      </Button>
-      <Button className="sidebar-button"
-        variant="contained"
-        sx={{ m: 0.5 }}
-        component={Link}
-        to="/login"
-        label="Login"
-        value="login"
-      >
-        Login
-      </Button>
-      <Button className="sidebar-button"
-        variant="contained"
-        sx={{ m: 0.5 }}
-        component={Link}
-        to="/myrecipes"
-        label="MyRecipes"
-        value="myrecipes"
-      >
-        My Recipes
-      </Button>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Paper className="sidebar" elevation={40} background square>
+        <Button className="sidebar-button"
+          variant="contained"
+          sx={{ m: 0.5 }}
+          component={Link}
+          to="/"
+          label="Home"
+          value="home"
+        >
+          Home
+        </Button>
+        <Button className="sidebar-button"
+          variant="contained"
+          sx={{ m: 0.5 }}
+          component={Link}
+          to="/login"
+          label="Login"
+          value="login"
+        >
+          Login
+        </Button>
+        <Button className="sidebar-button"
+          variant="contained"
+          sx={{ m: 0.5 }}
+          component={Link}
+          to="/myrecipes"
+          label="MyRecipes"
+          value="myrecipes"
+        >
+          My Recipes
+        </Button>
+      </Paper>
+    </ThemeProvider>
   )
 }
 
